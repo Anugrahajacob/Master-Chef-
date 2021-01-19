2ffcae3a866303377a4c0ea2e62bc1a57aea0839
Anugrahajacob <75684857+Anugrahajacob@users.noreply.github.com>
2021-01-19 21:15:09 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,34 @@
+class paper{
+
+    constructor(x,y,radius){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.body=Bodies.circle(this.x,this.y,this.radius/2,options);
+           this.radius=radius;
+           World.add(world,this.body);
+           this.image = loadImage("sprites/paper.png");
+
+    }
+
+
+    display(){
+        var pos =this.body.position
+        push ()
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        strokeWeight(5);
+        stroke("blue");
+        fill (225,0,225);
+         ellipse(0,0,this.r,this.r);
+         image(this.image,this.body.position.x,this.body.position.y,x,y);
+        pop ();
+    }
+
+}
